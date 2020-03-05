e770903ffb7ee0180569b53af821322d65e107a8
Grzegorz Paprzycki <paprzycki.grzegorz@gmail.com>
2020-03-05 20:14:39 +0100

    Updates

---
+++ precache-manifest.8457429065c6de37612c5e019564593e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab671c5a53c7b496dc40ed24dfd0e0f6",
+    "url": "/Phrase-Finder-React/index.html"
+  },
+  {
+    "revision": "c151edc013b0b9df32e1",
+    "url": "/Phrase-Finder-React/static/css/main.bc221462.chunk.css"
+  },
+  {
+    "revision": "48dcfc35f54e3389ef0e",
+    "url": "/Phrase-Finder-React/static/js/2.d7cc4b2b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Phrase-Finder-React/static/js/2.d7cc4b2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c151edc013b0b9df32e1",
+    "url": "/Phrase-Finder-React/static/js/main.affdcffd.chunk.js"
+  },
+  {
+    "revision": "a3241f97bdd3c0973ca4",
+    "url": "/Phrase-Finder-React/static/js/runtime-main.5b8658af.js"
+  },
+  {
+    "revision": "8614d81bde334310bf9c2a3972e03e1b",
+    "url": "/Phrase-Finder-React/static/media/typewriter.8614d81b.jpg"
+  }
+]);
